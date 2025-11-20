132d2d98af798ea111f4d6bbbd3e4625a72919b8
Irrfan <kaungkhant12359@gmail.com>
2025-11-21 01:30:46 +0800

    4th commit

--- firstApp/src/App.tsx
+++ firstApp/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        This is my new app. and this will be very big
+        This is my new app. and this will be very big. will have too many features.
       </p>
     </>
   )
